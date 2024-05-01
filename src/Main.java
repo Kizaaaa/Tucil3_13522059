ea9db6f6fddbea33ffc1442b9b9588bbaf2fcf47
Kiza <themightyomegagd@gmail.com>
2024-05-01 13:24:09 +0700

    input validation and dict fix

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
     public static Set<String> Dictionary;
 
-    public static boolean isWordValid(String s){
-        if(s.indexOf(" ") != -1){
-            return false;
+    public static void readDictionary(String filePath) {
+        Dictionary = new HashSet<String>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Dictionary.add(line.trim()); // trim() to remove any leading or trailing whitespaces
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading from file: " + e.getMessage());
         }
-        return true;
     }
+    
     public static void main(String[] args) {
+        // Deklarasi set untuk menyimpan kata inggris yang valid
+        readDictionary("dict.txt");
+
+        // Deklarasi Scanner untuk input
         Scanner sc = new Scanner(System.in);
         
-
+        // Input dan validasi kata awal
         System.out.print("Masukkan Kata Awal : ");
         String start = sc.nextLine();
-        while(!isWordValid(start)){
+        while(!Dictionary.contains(start)){
             System.out.print("\nKata tidak valid!\n\nMasukkan Kata Awal : ");
             start = sc.nextLine();
         }
 
+        // Input dan validasi kata akhir
         System.out.print("Masukkan Kata Akhir : ");
         String end = sc.nextLine();
-        while(!isWordValid(end) && start.length() != end.length()){
+        while(!Dictionary.contains(end) || start.length() != end.length()){
             if(start.length() != end.length()){
                 System.out.println("\nPanjang kata berbeda!");
             } else {
@@ -34,7 +49,7 @@ public class Main {
         }
 
         
-
+        // Close Scanner
         sc.close();
     }
 }
