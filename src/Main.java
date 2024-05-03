0a0ff9b2ea35fe90df3cee23be2034c0912f0f79
Kiza <themightyomegagd@gmail.com>
2024-05-03 11:55:29 +0700

    feat:A star

--- src/Main.java
+++ src/Main.java
@@ -32,6 +32,7 @@ public class Main {
 
         // Memasukkan node awal pada queue
         queue.add(t);
+        visited.add(start);
 
         // Memulai proses UCS
         while(true){
@@ -67,7 +68,7 @@ public class Main {
         // Menghentikan perhitungan waktu
         endTime = System.currentTimeMillis();
 
-        // Mengembalikan jalur yang telah ditemukan
+        // Mencatat jalur yang telah ditemukan
         path = queue.get(0).path;
     }
 
@@ -82,20 +83,23 @@ public class Main {
     }
 
     public static void InsertGBFS(Tree t){
-        if(queue.isEmpty() || queue.get(queue.size()-1).h < t.h){
+        if(queue.isEmpty() || queue.get(queue.size()-1).h <= t.h){
             queue.add(t);
-        }
-        for(int i=0;i<queue.size();i++){
-            if(queue.get(i).kata == t.kata){
-                break;
-            }
-            if(queue.get(i).h > t.h){
-                queue.add(i, t);
-                for(int j=i+1;j<queue.size();j++){
-                    if(queue.get(j).kata == t.kata){
-                        queue.remove(j);
-                    }
+        } else {
+            for(int i=0;i<queue.size();i++){
+                if(queue.get(i).h > t.h){
+                    queue.add(i, t);
+                    break;
                 }
+            }
+        }
+    }
+
+    public static void Delete(String s){
+        for(int i=0;i<queue.size();i++){
+            if(queue.get(i).kata.equals(s)){
+                queue.remove(i);
+                break;
             }
         }
     }
@@ -109,49 +113,108 @@ public class Main {
 
         // Memasukkan node awal pada queue
         queue.add(t);
+        visited.add(start);
 
         // Memulai proses UCS
         while(true){
             // Menambah node yang dikunjungi
             dikunjungi++;
-            System.out.println(queue.get(0).kata);
+            Tree treeSekarang = queue.get(0);
 
             // Kata tujuan ditemukan! keluar dari while loop
-            if(queue.get(0).kata.equals(end)){
+            if(treeSekarang.kata.equals(end)){
                 break;
             }
 
             // Menghasilkan kata yang berbeda 1 huruf
             for(int i=0;i<len;i++){
                 for(char c : "abcdefghijklmnopqrstuvwxyz".toCharArray()){
-                    if(queue.get(0).kata.charAt(i) != c){
-                        String tempString = queue.get(0).kata.substring(0, i) + c + queue.get(0).kata.substring(i+1);
-                        System.out.println(tempString);
-                        // Jika kata valid
-                        if(dictionary.contains(tempString)){
-                            ArrayList<String> tempPath = new ArrayList<>(queue.get(0).path);
+                    if(treeSekarang.kata.charAt(i) != c){
+                        String tempString = treeSekarang.kata.substring(0, i) + c + treeSekarang.kata.substring(i+1);
+                        // Jika kata valid dan belum dilewati
+                        if(!visited.contains(tempString) && dictionary.contains(tempString)){
+                            ArrayList<String> tempPath = new ArrayList<>(treeSekarang.path);
                             tempPath.add(tempString);
                             Tree tempTree = new Tree(0,calculateDistanceToFinish(tempString), tempString, tempPath);
                             InsertGBFS(tempTree);
+                            visited.add(tempString);
                         }
                     }
                 }
             }
-            System.out.println("sini");
+
             // Menghapus kata yang sudah dicek dari queue
-            queue.remove(0);
+            Delete(treeSekarang.kata);
         }
 
         // Menghentikan perhitungan waktu
         endTime = System.currentTimeMillis();
+
+        // Mencatat jalur yang telah ditemukan
+        path = queue.get(0).path;
+    }
+
+    public static void InsertAS(Tree t){
+        if(queue.isEmpty() || queue.get(queue.size()-1).h + queue.get(queue.size()-1).g <= t.h + t.g){
+            queue.add(t);
+        } else {
+            for(int i=0;i<queue.size();i++){
+                if(queue.get(i).h + queue.get(i).g > t.h + t.g){
+                    queue.add(i, t);
+                    break;
+                }
+            }
+        }
     }
 
     public static void AS(){
         // Memulai perhitungan waktu
         startTime = System.currentTimeMillis();
 
+        // Deklarasi node awal
+        Tree t = new Tree(0, calculateDistanceToFinish(start), start, new ArrayList<String>());
+
+        // Memasukkan node awal pada queue
+        queue.add(t);
+        visited.add(start);
+
+        // Memulai proses UCS
+        while(true){
+            // Menambah node yang dikunjungi
+            dikunjungi++;
+            Tree treeSekarang = queue.get(0);
+
+            // Kata tujuan ditemukan! keluar dari while loop
+            if(treeSekarang.kata.equals(end)){
+                break;
+            }
+
+            // Menghasilkan kata yang berbeda 1 huruf
+            for(int i=0;i<len;i++){
+                for(char c : "abcdefghijklmnopqrstuvwxyz".toCharArray()){
+                    if(treeSekarang.kata.charAt(i) != c){
+                        String tempString = treeSekarang.kata.substring(0, i) + c + treeSekarang.kata.substring(i+1);
+                        // Jika kata valid dan belum dilewati
+                        if(!visited.contains(tempString) && dictionary.contains(tempString)){
+                            ArrayList<String> tempPath = new ArrayList<>(treeSekarang.path);
+                            tempPath.add(tempString);
+                            Tree tempTree = new Tree(treeSekarang.g+1,calculateDistanceToFinish(tempString), tempString, tempPath);
+                            InsertAS(tempTree);
+                            visited.add(tempString);
+                        }
+                    }
+                }
+            }
+
+            // Menghapus kata yang sudah dicek dari queue
+            Delete(treeSekarang.kata);
+        }
+
         // Menghentikan perhitungan waktu
         endTime = System.currentTimeMillis();
+
+        // Mencatat jalur yang telah ditemukan
+        path = queue.get(0).path;
     }
     
     public static void main(String[] args) {
@@ -199,29 +262,34 @@ public class Main {
         // Close Scanner
         sc.close();
 
-        // Pemanggilan fungsi algoritma sesuai input
-        switch (algo) {
-            case "1": // Uniform Cost Search (UCS)
-                UCS();
-                break;
-            case "2": // Greedy Best-First Search
-                GBFS();
-                break;
-            case "3": // A* Search
-                AS();
-                break;
-        }
-
-        // Print Path
-        System.out.println("\nPath :");
-        System.out.println(start.toUpperCase());
-        if(start != end){
-            for(String s : path){
-                System.out.println(s.toUpperCase());
+        
+        try {
+            // Pemanggilan fungsi algoritma sesuai input
+            switch (algo) {
+                case "1": // Uniform Cost Search (UCS)
+                    UCS();
+                    break;
+                case "2": // Greedy Best-First Search
+                    GBFS();
+                    break;
+                case "3": // A* Search
+                    AS();
+                    break;
             }
-        }
 
-        // Print node yang dikunjungi dan waktu yang diperlukan
-        System.out.println("\nPanjang path : " + path.size() + " Langkah\nNode yang dikunjungi : " + dikunjungi + "\nWaktu : " + (endTime-startTime) + " ms");
+            // Print Path
+            System.out.println("\nPath :");
+            System.out.println(start.toUpperCase());
+            if(start != end){
+                for(String s : path){
+                    System.out.println(s.toUpperCase());
+                }
+            }
+
+            // Print node yang dikunjungi dan waktu yang diperlukan
+            System.out.println("\nPanjang path : " + path.size() + " Langkah\nNode yang dikunjungi : " + dikunjungi + "\nWaktu : " + (endTime-startTime) + " ms");
+        } catch (IndexOutOfBoundsException exception) {
+            System.out.println("Tidak ditemukan path dari " + start + " menuju " + end + ".");
+        }        
     }
 }
